499ab2f0945b848f0f975a6e7ddf4c7e141657a3
the-kintsukuroi <91498475+the-kintsukuroi@users.noreply.github.com>
2021-12-17 18:58:13 -0500

    check

--- srcs/streamlit_app/app.py
+++ srcs/streamlit_app/app.py
@@ -1,8 +1,15 @@
 # srcs/streamlit_app/app.py
+import sys
 import streamlit as st
 import pandas as pd
-import numpy as np
-import requests, os
+
+from elasticsearch import Elasticsearch
+sys.path.append('srcs')
+from streamlit_app import utils, templates
+
+INDEX = 'qp-name'
+DOMAIN = '0.0.0.0'
+es = Elasticsearch(host=DOMAIN)
 
 # -- Set page config
 apptitle = 'Jobs for You'
@@ -16,6 +23,8 @@ st.set_page_config(
 ) #TODO page_icon and menu_items
 
 # Title the app
+logo_url = 'https://cdn3.vectorstock.com/i/1000x1000/50/27/job-search-recruitment-hiring-employment-vector-21795027.jpg'
+st.image(logo_url, width=100)
 st.title(apptitle)
 
 # Define the education levels
@@ -31,6 +40,7 @@ education_level_def = { 'Education Level':['Unable to read or write', 'No formal
      'completed a PhD degree in any field', 'Other']
 }
 education_level_def = pd.DataFrame(education_level_def)
+
 sector_def = {'Sector':['Aerospace & Aviation', 'Agriculture',
        'Apparel, Made-Ups & Home Furnishing', 'Automotive',
        'Beauty & Wellness', 'Banking, Financial Services and Insurance',
@@ -52,39 +62,57 @@ sector_def = pd.DataFrame(sector_def)
 # Part 1 Get Details expander
 get_details_expander = st.expander("Enter Details", expanded=False)
 with get_details_expander:
-
     # Request Input using form
     input_form = st.form("input_form")
-    first_name = input_form.text_input(label='First Name')
-    last_name = input_form.text_input(label='Last Name')
-    email_address = input_form.text_input(label='Email Address')
+    first_name = input_form.text_input(label='First Name', value= 'Jane')
+    last_name = input_form.text_input(label='Last Name', value= 'Doe')
+    email_address = input_form.text_input(label='Email Address', value= 'janedoe@gmail.com')
     birth_date = input_form.date_input(label='Birth Date')
-    input_form.form_submit_button("Submit")
+    submit_input_form = input_form.form_submit_button("Submit")
 
-get_sector_expander = st.expander("Select the industry sector", expanded=False)
-with get_sector_expander:
-
-    # Request sector using form
-    sector_form= st.form("sector_selected")
-    sector_selected = sector_form.selectbox(
-        'Please select the sectors you are interested to work in:', sector_def
-    )
-    sector_form.form_submit_button("Submit")
-
+if submit_input_form:
+    st.sidebar.title("{} {}".format(first_name, last_name))    
+    st.sidebar.subheader(email_address)
+    age = utils.calculateAge(birth_date)
+    st.sidebar.write("Age: {} years".format(age))
+    #TODO collapse the expander after submission
 
 get_education_expander = st.expander("Provide your highest level of education", expanded=False)
 with get_education_expander:
-
     # Request educational qualification using form
     education_form = st.form("education_form")
     education_level = education_form.selectbox(
-     'Please select the highest level of your education(Refer to the table below):', education_level_def['Education Level']      
-     )
-    education_form.text_input(label='Please specify your Specialization:')
-    education_form.text_input(label='If Other, please specify:')
-    education_form.form_submit_button("Submit")
-    education_form.write('Please select the highest cleared level')
+     'Please select the highest level of your education(Refer to the table below):', education_level_def['Education Level'])
     education_form.dataframe(education_level_def)
+    #education_form.text_input(label='Please specify your Specialization:')
+    #education_form.text_input(label='If Other, please specify:')
+    submit_education_form = education_form.form_submit_button("Submit")
 
+if submit_education_form:
+    st.sidebar.write("Education: {}".format(education_level))
 
-    
+get_sector_expander = st.expander("Select the industry sector", expanded=False)
+with get_sector_expander:
+    # Request sector using form
+    sector_form= st.form("sector_selected")
+    sector_selected = sector_form.radio(
+        'Please select the sector you are interested to work in:', sector_def)
+    submit_sector_form = sector_form.form_submit_button("Submit")
+
+if submit_sector_form:
+    st.sidebar.write("Sector:{}".format(sector_selected))
+
+get_occupations_expander = st.expander("Check out these occupations", expanded=True)
+with get_occupations_expander:
+    results = utils.index_search(education_level, sector_selected)
+    total_hits = len(results['hits']['hits'])
+    placeholder = st.empty()
+    # show number of results and time taken
+    with placeholder.container():
+        st.write(templates.number_of_results(total_hits, results['took'] / 1000), unsafe_allow_html=True)
+        # search results
+        for i in range(len(results['hits']['hits'])):
+            result = results['hits']['hits'][i]
+            res = result['_source']['QP Name']
+            st.write(templates.search_result(i, res), unsafe_allow_html=True)
+
